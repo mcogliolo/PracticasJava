43b08efc92f2d05caec4145407c616126cab3e99
Mariela Cogliolo <mariela.cogliolo@cision.com>
2018-04-19 20:08:41 -0300

    Add ejer7 y ejer8 por terminar

--- src/main/java/org/ejercicios/practico1/Ejercicio7.java
+++ src/main/java/org/ejercicios/practico1/Ejercicio7.java
@@ -2,6 +2,8 @@ package org.ejercicios.practico1;
 
 import java.util.Scanner;
 
+import javax.swing.SingleSelectionModel;
+
 /**
  * @author Mariela Cogliolo
  * Ejercicio n° 7
@@ -14,14 +16,27 @@ import java.util.Scanner;
 public class Ejercicio7 {
 	public static void main(String[] args){
 		
-		Scanner radio = new Scanner(System.in);
 		
-		System.out.print("Escriba dos números dobles: ");
+		int respuesta = 1;
+		
+		while (respuesta == 1) {		
+		Scanner radio = new Scanner(System.in);
+		System.out.print("Escriba el radio del perimetro a calcular: ");
 		double rad = radio.nextDouble();
 		
 		double perCirc = 2* Math.PI * rad;
 		
 		System.out.println("El perimetro de la cicunferencia con radio de "
-				+ rad + " es de: " + perCirc);		
+				+ rad + " es de: " + perCirc);	
+		
+		System.out.print(" ");
+		
+		Scanner ask = new Scanner(System.in);
+		System.out.print("Desea continuar calculando un perimetro 1/2: ");
+		int respuesta2 = ask.nextInt();
+		respuesta = respuesta2;
+		
+		}		
+		System.out.print("MUCHAS GRACIAS");		
 	}
 }
---
+++ src/main/java/org/ejercicios/practico1/Ejercicio8.java
@@ -0,0 +1,12 @@
+package org.ejercicios.practico1;
+/**
+ * Ejercicio n°8:
+ *  Crear una clase denominada OperarVector que permita ingresar por teclado (Scanner) 5 notas de alumnos, las
+que serán almacenadas en un array de enteros. Calcular el promedio y determinar la mayor nota. El promedio
+debe permitir resultado con decimales (aplicar cast a los elementos enteros cuando sea necesario para obtener
+dicho resultado). Mostrar los elementos ingresados, separados por un tabulador. Mostrar el promedio y la
+mayor nota con el mensaje respectivo.
+ */
+public class Ejercicio8 {
+
+}
