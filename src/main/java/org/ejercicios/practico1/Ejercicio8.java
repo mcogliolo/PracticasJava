f18c804a02aef0ea40e00829abf4d93daed2519a
Mariela Cogliolo <mariela.cogliolo@cision.com>
2018-05-09 20:19:31 -0300

    un poco mas

--- src/main/java/org/ejercicios/practico1/Ejercicio8.java
+++ src/main/java/org/ejercicios/practico1/Ejercicio8.java
@@ -5,37 +5,66 @@ import java.util.Scanner;
 /**
  * Ejercicio n°8:
  *  Crear una clase denominada OperarVector que permita ingresar por teclado (Scanner) 5 notas de alumnos, las
-que serán almacenadas en un array de enteros. Calcular el promedio y determinar la mayor nota. El promedio
-debe permitir resultado con decimales (aplicar cast a los elementos enteros cuando sea necesario para obtener
-dicho resultado). Mostrar los elementos ingresados, separados por un tabulador. Mostrar el promedio y la
-mayor nota con el mensaje respectivo.
+ * que serán almacenadas en un array de enteros. Calcular el promedio y determinar la mayor nota. El promedio
+ * debe permitir resultado con decimales (aplicar cast a los elementos enteros cuando sea necesario para obtener
+ * dicho resultado). Mostrar los elementos ingresados, separados por un tabulador. Mostrar el promedio y la
+ * mayor nota con el mensaje respectivo.
  */
 public class Ejercicio8 {
 	public static void main(String[] args){
 	
-	int  = 1;
+	//Scanner nota = new Scanner(System.in);
+	int[] nota = new int[4];
 	
-	Double[] = notas; 
+	nota[0] = 5;
+	nota[1] = 5;
+	nota[2] = 5;
+	nota[3] = 5;
+	nota[4] = 5;
 	
-	while (respuesta == 1) {		
-		Scanner radio = new Scanner(System.in);
-		System.out.print("Escriba el radio del perimetro a calcular: ");
-		double rad = radio.nextDouble();
+	double promedio; 
+	for(int i=0; i<nota.length; i++){
+				
+		promedio = (nota[i] + nota [i++])/nota.length;
+	}
+		
 	
-		double perCirc = 2* Math.PI * rad;
 	
-		System.out.println("El perimetro de la cicunferencia con radio de "
-				+ rad + " es de: " + perCirc);	
+	System.out.print("Promedio" + promedio);
 	
-		System.out.print(" ");
-	
-		Scanner ask = new Scanner(System.in);
-		System.out.print("Desea continuar calculando un perimetro 1/2: ");
-		int respuesta2 = ask.nextInt();
-		respuesta = respuesta2;
-	
-	}		
-	System.out.print("MUCHAS GRACIAS");		
-}
+//	
+//	int[] a = { nota1 , nota2 , nota3, nota4, nota5 };
+//	
+//	int nota1 = Double.parseDouble(args[0]);
+//	int nota2 = nota.nextDouble();
+//	int nota3 = nota.nextDouble();
+//	int nota4 = nota.nextDouble();
+//	int nota5 = nota.nextDouble();
+//	
+//
+//	int[] a = { nota1 , nota2 , nota3, nota4, nota5 };
+//	
+//	
+//	while (respuesta == 1) {		
+//		Scanner radio = new Scanner(System.in);
+//		System.out.print("Escriba el radio del perimetro a calcular: ");
+//		double rad = radio.nextDouble();
+//	
+//		double perCirc = 2* Math.PI * rad;
+//	
+//		System.out.println("El perimetro de la cicunferencia con radio de "
+//				+ rad + " es de: " + perCirc);	
+//	
+//		System.out.print(" ");
+//	
+//		Scanner ask = new Scanner(System.in);
+//		System.out.print("Desea continuar calculando un perimetro 1/2: ");
+//		int respuesta2 = ask.nextInt();
+//		respuesta = respuesta2;
+//	
+//	}		
+//	System.out.print("MUCHAS GRACIAS");		
+//}
+	}
 
 }
