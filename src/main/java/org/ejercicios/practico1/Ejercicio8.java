bc878f217b100c1656e19acbcdf5dfa6367bbaea
Mariela cogliolo <mariela.cogliolo@gmail.com>
2018-05-13 23:25:23 -0300

    Adding Ejercicio 8

--- src/main/java/org/ejercicios/practico1/Ejercicio8.java
+++ src/main/java/org/ejercicios/practico1/Ejercicio8.java
@@ -12,59 +12,29 @@ import java.util.Scanner;
  */
 public class Ejercicio8 {
 	public static void main(String[] args){
-	
-	//Scanner nota = new Scanner(System.in);
-	int[] nota = new int[4];
-	
-	nota[0] = 5;
-	nota[1] = 5;
-	nota[2] = 5;
-	nota[3] = 5;
-	nota[4] = 5;
-	
-	double promedio; 
-	for(int i=0; i<nota.length; i++){
-				
-		promedio = (nota[i] + nota [i++])/nota.length;
-	}
+		Scanner notasx = new Scanner(System.in);
 		
-	
-	
-	System.out.print("Promedio" + promedio);
-	
-//	
-//	int[] a = { nota1 , nota2 , nota3, nota4, nota5 };
-//	
-//	int nota1 = Double.parseDouble(args[0]);
-//	int nota2 = nota.nextDouble();
-//	int nota3 = nota.nextDouble();
-//	int nota4 = nota.nextDouble();
-//	int nota5 = nota.nextDouble();
-//	
-//
-//	int[] a = { nota1 , nota2 , nota3, nota4, nota5 };
-//	
-//	
-//	while (respuesta == 1) {		
-//		Scanner radio = new Scanner(System.in);
-//		System.out.print("Escriba el radio del perimetro a calcular: ");
-//		double rad = radio.nextDouble();
-//	
-//		double perCirc = 2* Math.PI * rad;
-//	
-//		System.out.println("El perimetro de la cicunferencia con radio de "
-//				+ rad + " es de: " + perCirc);	
-//	
-//		System.out.print(" ");
-//	
-//		Scanner ask = new Scanner(System.in);
-//		System.out.print("Desea continuar calculando un perimetro 1/2: ");
-//		int respuesta2 = ask.nextInt();
-//		respuesta = respuesta2;
-//	
-//	}		
-//	System.out.print("MUCHAS GRACIAS");		
-//}
-	}
+		int[] notas = new int[5];		
+		for(int i=0; i<notas.length;i++){
+			System.out.print("Ingrese la nota: ");
+			notas[i] = Integer.valueOf(notasx.nextInt());
+		}
+		int suma = 0;
+		int mayorNota = notas[0];
+		System.out.print("Las notas son las siguiente: " );
+		for(int i=0; i<notas.length;i++){
+			suma = suma + notas[i];
+			if(notas[i]>mayorNota){
+				mayorNota = notas[i];
+			}
+			System.out.print(notas[i]+"|");
+		}
+		double promedio = suma/notas.length;
 
+		System.out.println("\nEl promedio es: " + promedio);
+		System.out.println("La Mayor nota es: " + mayorNota);
+		}
 }
+
+		
+		
